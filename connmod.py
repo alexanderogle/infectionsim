e847f9821cdd5905e0ca487a74409665a6783e1d
Joelle Murray <jmurray@linfield.edu>
2020-04-30 10:24:53 -0700

    i totally still don't understand what i'm doing, but i think i pushed these 2 files from my local to alex's repository. 1. codeSIR.py is a stand alone program that uses coupled linear ODEs to find the compartment populations in a simple SIR model. I'll rewrite it as a function. 2. connmod.py is a simple function that calculates the number of connections an individual has based on the population density and the interaction area. I'll probably change it to number of steps in a day and distance within infection is possible (social distancing recommended distance).
    Please don't laugh too hard at the old lady learning new tricks - and thank you guys for all of your patience and guidance. You both would make excellent teachers some day if you ever feel like having a satisfying job that doesn't pay you very well. Hahaha. I'm pretty sure these summaries aren't supposed to be this long. sorry!

---
+++ connmod.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 18:47:41 2020
+
+@author: jmurray
+"""
+# connection model function
+# return number of connections/(person*day)
+# parameters: population density (popdens), interaction area (intarea)
+# interaction area = 12 ft wide by distance walked in one day
+# average steps walked is ~4800 steps
+#   Althoff, T., Sosič, R., Hicks, J. et al. Large-scale physical activity 
+#   data reveal worldwide activity inequality. Nature 547, 336–339 (2017). 
+#   https://doi.org/10.1038/nature23018
+# average distance is ~2.2 miles (based on a 2.5 ft stride and the # of steps above)
+# interaction area is units of square miles 
+# 0.005 mi^2=12ft*2.2mi*1mi/5280ft
+# population density numbers (people/mi^2)
+# NYC:270,000; San Fran: 17,000; Chicago: 12,000; Ketchikan: 2,300
+#   https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population_density
+#   based on 2010 census
+# later: demographics, social distancing and policy modifications
+def connmod(popdens,intarea):
+    numconn = intarea*popdens
+    return round(numconn)
